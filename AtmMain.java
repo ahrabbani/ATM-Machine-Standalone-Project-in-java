4031894a4e0e63705ce51b3f39c93e7dfccee507
AARIF HUSSAIN RABBANI <153491898+ahrabbani@users.noreply.github.com>
2025-08-28 22:36:15 +0530

    Update AtmMain.java

--- AtmMain.java
+++ AtmMain.java
@@ -10,10 +10,10 @@ public class AtmMain {
 	public static void main(String[] args) {
 		
 		AtmOperationInterf op=new AtmOperationImpl();
-		int atmnumber=12345;
-		int atmpin=123;
+		int atmnumber=123456;
+		int atmpin=1234;
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Welcome to ATM Machine!");
+		System.out.println("***Welcome to ATM Machine!***");
 		System.out.print("Enter ATM number: ");
 		System.out.printn();
 		int atmnumber1=sc.nextInt();
@@ -68,6 +68,7 @@ public class AtmMain {
 	}
 
 }
+
 
 
 
